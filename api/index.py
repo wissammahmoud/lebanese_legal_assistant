1e0acb19a2e91a385f7af86009c9db8ee5381fbf
wissammahmoud <wissam@okation.ai>
2026-02-26 17:51:56 +0200

    Updating Vercel

--- api/index.py
+++ api/index.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the root directory to the sys.path so 'app' can be found
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
 from app.main import app
 
-# This allows Vercel to find the FastAPI instance
+# This is what Vercel will run
